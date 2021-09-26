c8f3282d8d81a8206fdb1370cf61fb5f80ee8eab
olingomaxwell1999 <69717524+olingomaxwell1999@users.noreply.github.com>
2021-09-26 04:02:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let more = document.querySelectorAll('.more');
+
+for (let i = 0; i<more.length; i++){
+    more[i].addEventListener('click', function(){
+        more[i].parentNode.classList.toggle('active')
+    })
+}
